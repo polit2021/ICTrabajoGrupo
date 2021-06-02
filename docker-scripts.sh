ea72d85a67a6a4c5bff7e21bcd0b9e5992d3b003
Mario Daniel Ruiz Saavedra <desiderantes93@gmail.com>
2021-06-01 23:20:56 -0500

    Actualizar comentario

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -5,9 +5,10 @@ docker run -d --name poligran-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=poligran
 
 # Creamos el container de phpMyAdmin
 # 
-# PMA_HOST es el dominio de la instancia de MySQL a la que se va a conectar
+# --link permite acceso a la instancia de MySQL a la que se va a conectar
 # según https://hub.docker.com/r/phpmyadmin/phpmyadmin/
-# le pasamos el nombre porque así lo va a ver dentro de la red
+# le pasamos el nombre con ":db" porque así lo va a ver dentro de la red
+# ya que el contenedor de MySQL expone la db como un subrecurso con el nombre "db"
 docker run -d \
     --name poligran-phpmyadmin \
     --link=poligran-mysql:db \
