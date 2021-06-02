19a74ea683c2b53a37d9ae560869f075eb6894e2
Mario Daniel Ruiz Saavedra <desiderantes93@gmail.com>
2021-06-01 22:55:18 -0500

    Simplificar conexión entre contenedores
    Usamos --link en lugar de crear una network conjunta

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
-
-#Primero creamos una red por la que se van a comunicar ambos contenedores
-docker network create poligran
- 
-# Creamos el contenedor de MySQL 
-docker run -d \
-    --name poligran-mysql \
-    --network poligran \
-    -e MYSQL_ROOT_PASSWORD="poligran2021" \
-    -e MYSQL_USER="poligran"\
-    -p 3306:3306 \
-    mysql:latest
+# Creamos el container de MySQL 
+docker run -d --name poligran-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=poligran mysql:latest
 
 
-# Creamos el contenedor de phpMyAdmin
+# Creamos el container de phpMyAdmin
 # 
 # PMA_HOST es el dominio de la instancia de MySQL a la que se va a conectar
 # según https://hub.docker.com/r/phpmyadmin/phpmyadmin/
 # le pasamos el nombre porque así lo va a ver dentro de la red
 docker run -d \
     --name poligran-phpmyadmin \
-    --network poligran \
-    -e PMA_HOST=poligran-mysql \
+    --link=poligran-mysql \
     -p 8080:80 \
     phpmyadmin/phpmyadmin:latest
+
